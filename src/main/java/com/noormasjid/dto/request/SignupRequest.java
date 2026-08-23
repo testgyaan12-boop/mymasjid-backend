0df7fe047d2dc98df6b49f519f1fd6b89c9d41b6
majid <majid.chaudhary@finhaat.in>
2026-08-23 20:39:05 +0530

    login fix

--- src/main/java/com/noormasjid/dto/request/SignupRequest.java
+++ src/main/java/com/noormasjid/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package com.noormasjid.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,9 +21,12 @@ public class SignupRequest {
     private String email;
 
     @NotBlank
-    @Size(min = 6, max = 255)
+    @Size(min = 8, max = 255, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+             message = "Password must have uppercase, lowercase, number and special @$!%*?&")
     private String password;
 
+    @Pattern(regexp = "^$|^\\+?[0-9]{10,15}$", message = "Mobile must be 10-15 digits")
     @Size(max = 20)
     private String phone;
 }
